4da8d59993dbb6da1c0ab19efd13672894c00958
cameronjacoby <cameron.jacoby@gmail.com>
2014-08-20 21:28:20 -0700

    implement new post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,16 @@ class PostsController < ApplicationController
     render :index
   end
 
+  def new
+    render :new
+  end
+
+  def create
+    post = params.require(:post).permit(:title, :author, :content, :tagline, :image)
+    Post.create(post)
+    redirect_to '/posts'
+  end
+
   def show
     id = params[:id]
     @post = Post.find(id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   get '/posts', to: 'posts#index'
+  get '/posts/new', to: 'posts#new'
+  post '/posts', to: 'posts#create'
   get '/posts/:id', to: 'posts#show'
   get '/tags/:id', to: 'tags#show'
 end
