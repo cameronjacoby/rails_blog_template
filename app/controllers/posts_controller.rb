f9dcba222e19bd40fa17d581fd2f6b1c1f976b3d
cameronjacoby <cameron.jacoby@gmail.com>
2014-08-21 18:12:28 -0700

    implement update & delete methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,11 @@ class PostsController < ApplicationController
     @tags = Tag.all
   end
 
+
   def new
     @tags = Tag.all
   end
+
 
   def create
     post_params = params.require(:post).permit(:title, :author, :content, :tagline, :image)
@@ -16,19 +18,56 @@ class PostsController < ApplicationController
     new_post = Post.create(post_params)
 
     tag_params.each do |tag_str|
-      tag = Tag.find_by_name(tag_str)
-      if tag == nil
-        tag = Tag.create(name: tag_str)
-      end
+      tag = Tag.find_or_create_by(name: tag_str)
       new_post.tags << tag
     end
 
     redirect_to '/posts'
   end
 
+
   def show
     post_id = params[:id]
     @post = Post.find(post_id)
     @tags = Tag.all
+  end
+
+
+  def edit
+    post_id = params[:id]
+    @post = Post.find(post_id)
+    @tags = Tag.all
+  end
+
+
+  def update
+    post_id = params[:id]
+    post_params = params.require(:post).permit(:title, :author, :content, :tagline, :image)
+    tag_params = params.require(:tags).split(",").map(&:strip).map(&:downcase)
+
+    post = Post.find_by_id(post_id)
+    if post
+      post.update_attributes(post_params)
+      # remove all old tags
+      post.tags.clear
+      # add new tags
+      tag_params.each do |tag_str|
+        tag = Tag.find_or_create_by(name: tag_str)
+        post.tags << tag
+      end
+    end
+
+    redirect_to "/posts/" + post_id
+  end
+
+
+  def destroy
+    post_id = params[:id]
+    post = Post.find_by_id(post_id)
+    if post
+      post.destroy
+    end
+
+    redirect_to '/posts'
   end
 end
