59d7ac09e92b58b72f252d469a3329ec88f4c000
cameronjacoby <cameron.jacoby@gmail.com>
2014-08-21 21:56:48 -0700

    implement delete btn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,4 +28,28 @@ class CommentsController < ApplicationController
       render action: 'new'
     end
   end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find_by_id(params[:id])
+    @tags = Tag.all
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find_by_id(params[:id])
+
+    if @comment.update(params.require(:comment).permit(:title, :author, :body))
+      redirect_to [@post, @comment]
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find_by_id(params[:id])
+    @comment.destroy
+    redirect_to post_comments_path(@post)
+  end
 end
